e3a2a61fad25fc28609d88fc3a08343343f7a214
Nguyễn Thanh Tiến Phát <nttp@netcompany.com>
2021-04-19 09:17:31 +0700

    add login function

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,11 +3,11 @@ import logo from "../asset/logo.svg";
 import "./NavBar.css";
 import storage from "local-storage-fallback";
 import DrawerToggleButton from "./DrawerToggleButton/DrawerToggleButton";
+import { useAuth0 } from '@auth0/auth0-react'
 
 const NavBar = ({ theme, changeTheme, drawerClickHandler }) => {
-  const [checked, setChecked] = useState(
-    storage.getItem("checked") === "true" ? false : true
-  );
+
+  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
   return (
     <div className="navbar">
       <DrawerToggleButton theme={theme} click={drawerClickHandler} />
@@ -18,7 +18,22 @@ const NavBar = ({ theme, changeTheme, drawerClickHandler }) => {
           <h4>SAFETY MAP</h4>
         </div>
       </div>
-      <div className="login"></div>
+      {isAuthenticated ?
+      <div className = 'login'>
+        <h5>
+          Welcome, {user.given_name} 
+        </h5>
+        <button type='button' onClick={() => logout({ returnTo: window.location.origin })} >
+          Logout
+      </button> 
+      </div>
+        :
+        <div className="login">
+          <button type='button' onClick={loginWithRedirect}>
+            Login
+      </button>
+        </div>
+      }
     </div>
   );
 };
--- src/context/userContext.js
+++ src/context/userContext.js
@@ -1,11 +1,21 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import {useAuth0} from '@auth0/auth0-react'
 
 const UserContext = React.createContext();
 
 export const UserProvider = ({children}) => {
+    const {isAuthenticated, loginWithRedirect, logout, user, isLoading} = useAuth0();
+    const [myUser, setMyUser] = useState(null);
+
+    useEffect(() => {
+        {
+            console.log(user)
+            console.log(`isAuthenticated: ${isAuthenticated}`)
+            console.log(`isLoading: ${isLoading}`)
+        }
+    }, [isAuthenticated])
     return (
-        <UserContext.Provider value ='user context'>
+        <UserContext.Provider value = {{loginWithRedirect, logout, myUser}}>
             {children}
         </UserContext.Provider>
     )
